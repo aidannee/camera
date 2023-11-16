d61745d538ca3dee1cf783407ce782dadf0efdff
aidannee <aidan.nee@students.beaminstitute.org>
2023-11-16 15:02:03 +0100

    draggable false added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="h-screen w-screen pb-28 md:pb-0 overflow-hidden">
+      <div
+        draggable="false"
+        className="h-screen w-screen pb-28 md:pb-0 overflow-hidden"
+      >
         {" "}
         <Profile />
       </div>
